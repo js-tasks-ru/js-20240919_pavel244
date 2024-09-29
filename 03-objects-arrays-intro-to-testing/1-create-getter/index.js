3d6d546343778daa720c40a100d9a15b9590aeff
Pavel Shakuro <sh.pash.v@gmail.com>
2024-09-29 18:43:11 +0300

    task 03/01

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,15 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  const keys = path.split('.');
+  let n = keys.length;
+  return (obj) => {
+    for (let i = 0; i < n; i++) {
+      if (!obj.hasOwnProperty(keys[i])) {
+        return undefined;
+      }
+      obj = obj[keys[i]];
+    }
+    return obj;
+  };
 }
