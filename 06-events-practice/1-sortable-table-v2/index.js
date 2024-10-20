cc7728e28c5a981632a3b7d27eb10b229849e5a4
Pavel Shakuro <sh.pash.v@gmail.com>
2024-10-20 23:55:03 +0300

    task 06/01

--- 06-events-practice/1-sortable-table-v2/index.js
+++ 06-events-practice/1-sortable-table-v2/index.js
@@ -1,8 +1,51 @@
-export default class SortableTable {
+import SortableTableV1 from "../../05-dom-document-loading/2-sortable-table-v1/index.js";
+
+export default class SortableTable extends SortableTableV1 {
+  arrowElement;
+
   constructor(headersConfig, {
     data = [],
-    sorted = {}
+    sorted = {},
   } = {}) {
+    super(headersConfig, data);
 
+    this.createListeners();
+    this.setArrowElement();
+
+    if (sorted.id) {
+      this.sort(sorted.id, sorted.order ? sorted.order : 'desc');
+      const sortedColumnHeader = this.subElements.header.querySelector(`[data-id=${sorted.id}]`);
+      sortedColumnHeader.insertAdjacentElement('beforeend', this.arrowElement);
+      sortedColumnHeader.dataset.order = sorted.order;
+    }
+  }
+
+  setArrowElement() {
+    const element = document.createElement("div");
+    element.innerHTML = this.createSortArrowTemplate();
+    this.arrowElement = element.firstElementChild;
+  }
+
+  createListeners() {
+    this.subElements.header.addEventListener('pointerdown', this.handleHeaderClack);
+  }
+
+  removeListeners() {
+    this.subElements.header.removeEventListener('pointerdown', this.handleHeaderClack);
+  }
+
+  handleHeaderClack = (event) => {
+    const columnHeader = event.target.closest('.sortable-table__cell');
+    if (columnHeader && columnHeader.dataset.sortable) {
+      const order = columnHeader.dataset.order === 'desc' ? 'asc' : 'desc';
+      columnHeader.dataset.order = order;
+      this.sort(columnHeader.dataset.id, order);
+      columnHeader.insertAdjacentElement('beforeend', this.arrowElement);
+    }
+  };
+
+  remove() {
+    this.removeListeners();
+    super.remove();
   }
 }
